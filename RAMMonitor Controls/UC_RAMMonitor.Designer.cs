ae7156136228503c81dd5905f49a6c15e24631a9
Lama-hup-Coder <lamahh04@gmail.com>
2026-05-05 15:51:33 -0700

    Build: RAM Monitor base logic and UI structure

---
+++ RAMMonitor Controls/UC_RAMMonitor.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SystemMonitorPro.NewFolder1
+{
+    partial class UC_RAMMonitor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnlBottom = new System.Windows.Forms.Panel();
+            this.lblStatusText = new System.Windows.Forms.Label();
+            this.lblUsedValue = new System.Windows.Forms.Label();
+            this.pnlBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(10, 10);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Padding = new System.Windows.Forms.Padding(10, 5, 0, 0);
+            this.lblTitle.Size = new System.Drawing.Size(138, 33);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "RAM USAGE";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pnlBottom
+            // 
+            this.pnlBottom.BackColor = System.Drawing.Color.Transparent;
+            this.pnlBottom.Controls.Add(this.lblUsedValue);
+            this.pnlBottom.Controls.Add(this.lblStatusText);
+            this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlBottom.Location = new System.Drawing.Point(10, 225);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.Padding = new System.Windows.Forms.Padding(15, 0, 0, 10);
+            this.pnlBottom.Size = new System.Drawing.Size(380, 65);
+            this.pnlBottom.TabIndex = 3;
+            // 
+            // lblStatusText
+            // 
+            this.lblStatusText.AutoSize = true;
+            this.lblStatusText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblStatusText.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatusText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.lblStatusText.Location = new System.Drawing.Point(15, 0);
+            this.lblStatusText.Name = "lblStatusText";
+            this.lblStatusText.Size = new System.Drawing.Size(144, 17);
+            this.lblStatusText.TabIndex = 0;
+            this.lblStatusText.Text = "RAM CURRENT USAGE";
+            this.lblStatusText.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblUsedValue
+            // 
+            this.lblUsedValue.AutoSize = true;
+            this.lblUsedValue.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsedValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblUsedValue.Location = new System.Drawing.Point(18, 17);
+            this.lblUsedValue.Name = "lblUsedValue";
+            this.lblUsedValue.Size = new System.Drawing.Size(119, 38);
+            this.lblUsedValue.TabIndex = 1;
+            this.lblUsedValue.Text = "0.00 GB";
+            // 
+            // UC_RAMMonitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(22)))), ((int)(((byte)(31)))));
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pnlBottom);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "UC_RAMMonitor";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Size = new System.Drawing.Size(400, 300);
+            this.pnlBottom.ResumeLayout(false);
+            this.pnlBottom.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel pnlBottom;
+        private System.Windows.Forms.Label lblUsedValue;
+        private System.Windows.Forms.Label lblStatusText;
+    }
+}
